450cda47f4e581222a79a6ff95f608ae71b6b64c
Daniel Parry <daniel_parry@cantab.net>
2010-08-06 12:51:05 +0100

    Initial commit of Sakai::Nakamura

---
+++ lib/Sakai/Nakamura.pm
@@ -0,0 +1,55 @@
+#!/usr/bin/perl -w
+package Sakai::Nakamura;
+
+use 5.008008;
+use strict;
+use warnings;
+
+require Exporter;
+
+use base qw(Exporter);
+
+our @EXPORT_OK = ();
+
+our $VERSION = '0.01';
+
+
+# Preloaded methods go here.
+
+1;
+__END__
+
+=head1 NAME
+
+Sakai::Nakamura - Perl library for interacting with the sakai nakamura web framework
+
+=head1 SYNOPSIS
+
+  use Sakai::Nakamura
+
+=head1 DESCRIPTION
+
+The Sakai::Nakamura perl library is designed to provide a perl based interface on
+to the Sakai::Nakamura web framework. 
+
+=head2 EXPORT
+
+None by default.
+
+=head1 SEE ALSO
+
+http://groups.google.co.uk/group/sakai-kernel
+
+=head1 AUTHOR
+
+D. D. Parry, E<lt>perl@ddp.me.ukE<gt>
+
+=head1 VERSION
+
+0.10
+
+=head1 LICENSE AND COPYRIGHT
+
+LICENSE: http://dev.perl.org/licenses/artistic.html
+
+COPYRIGHT: Daniel David Parry <perl@ddp.me.uk>
